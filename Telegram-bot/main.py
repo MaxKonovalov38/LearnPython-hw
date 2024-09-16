9366d246e44e9dee5d6bee95598d9a6ee6515c84
MaxKonovalov38 <max.kon.38r@gmail.com>
2024-09-16 15:42:01 +0800

    Добавлены настройки прокси

--- Telegram-bot/main.py
+++ Telegram-bot/main.py
@@ -1,6 +1,9 @@
 from telegram.ext import Updater
 import settings
 
+# Добавляем словарь со настройками прокси
+PROXY = {'proxy_url': 'socks5://t2.learn.python.ru:1080', 'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
+
 def main():
     mybot = Updater(settings.API_key, use_context=True)
     mybot.start_polling()
