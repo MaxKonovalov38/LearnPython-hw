acec5f3082449d0a702c4d065925c23afbc4853d
MaxKonovalov38 <max.kon.38r@gmail.com>
2024-09-16 21:59:03 +0800

    Простое рабочее веб-приложение

--- learn_web/main.py
+++ learn_web/main.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from weather import weather_by_city
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello!"
-
+def index():
+    weather = weather_by_city("Irkutsk,Russia")
+    if weather:
+        return f"Сейчас в Иркутске {weather['temp_C']} град., ощущается как {weather['FeelsLikeC']}"
+    else:
+        return "Прогноз сейчас недоступен."
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
