0e99550315a4899b6e9edd604745cc21e93edfcc
MaxKonovalov38 <max.kon.38r@gmail.com>
2024-09-16 16:42:03 +0800

    Домашняя работа выполнена

---
+++ learn-homework-1/4_while1.py
@@ -0,0 +1,20 @@
+"""
+Домашнее задание №1
+
+Цикл while: hello_user
+
+* Напишите функцию hello_user(), которая с помощью функции input() спрашивает 
+  пользователя “Как дела?”, пока он не ответит “Хорошо”
+   
+"""
+
+
+def hello_user():
+    while True:
+        answer = input('Как дела: ')
+        if answer == "Хорошо":
+            break
+
+    
+if __name__ == "__main__":
+    hello_user()
